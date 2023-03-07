d5a4f6e41055d0e5991d5e6af5ce00fce91e245a
Tarique Anwer Khan <110827286+Tariqueanwerkhan@users.noreply.github.com>
2023-03-07 09:38:01 +0530

    git add-A

---
+++ condition_Loops/src/loops.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+
+public class loops {
+    public static void main(String[] args) {
+        //int salary = 25400;
+//        if (salary > 10000){
+//            salary = salary + 2000;
+//        }
+//        else {
+//            salary = salary + 1000;
+//        }
+
+        //multiple if-else statement
+//        if(salary > 10000){
+//            salary += 2000; //same as salary = salary + 2000;
+//        }
+//        else if (salary > 20000){
+//            salary += 3000;
+//        }
+//        else{
+//            salary += 1000;
+//        }
+//        System.out.println(salary);
+
+        //Scanner in = new Scanner(System.in);
+
+        /*
+        syntax of for loops:
+
+        for (initialization; condition; increment/decrement){
+               //body
+        }
+         */
+
+        //print no. form 1 to 5
+//        for(int num = 1; num <= 5; num++){
+//            System.out.println(num);
+//        }
+
+        //print num from 1 to n .....
+//        Scanner in = new Scanner(System.in);
+//        int n = in.nextInt();
+//
+//        for (int num = 1; num <= n; num++) {
+//            //System.out.print(num + " ");
+//            System.out.println("Hello world");
+
+
+            //while loops
+            /*
+            syntax:
+            while (condition){
+                //body
+            }
+             */
+
+//            int num = 1;
+//            while(num <= 5){
+//                System.out.println(num);
+//                num++;
+//            }
+
+            //do while
+        /*
+        do{
+
+        }while(condition);
+         */
+        int n = 1;
+        do{
+            System.out.println(n);
+            n++;
+        } while(n <= 5);
+
+        }
+
+    }
+
